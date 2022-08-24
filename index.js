50ba97e8f7456eb96ef8b5518520bf1930c5a6de
Steve Hoffman <panningforbacon@gmail.com>
2022-08-24 12:24:45 -0500

    A simple oidc-provider implementation

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const { Provider } = require('oidc-provider');
 
 const issuer = 'http://localhost:3000';
 
-const oidcProvider = new Provider(issuer);
+const oidcProvider = new Provider(issuer, {
+    clients: [{
+        client_id: 'app',
+        client_secret: 'scorpion',
+        grant_types: ['authorization_code'],
+        redirect_uris: [`${issuer}/auth/login/callback`, 'https://oidcdebugger.com/debug'],
+        response_types: ['code'],
+    }],
+    pkce: {
+        required: () => false,
+    },
+});
 
 oidcProvider.listen(3000, () => {
     console.log(`Server listening on port 3000.`);
     console.log(`>> Discovery Document: ${issuer}/.well-known/openid-configuration`);
+    console.log(`>> Test the OP server: https://oidcdebugger.com/`);
 });
